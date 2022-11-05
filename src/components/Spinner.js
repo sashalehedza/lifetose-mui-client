c1eb305d58c2da092bc931f105c1d56d53321c92
Sasha Lehedza <sashalehedza@gmail.com>
2022-11-05 09:06:23 +0200

    fix

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -9,7 +9,7 @@ function Spinner() {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        width: '100vw',
+        width: '100%',
         height: '200px',
       }}
     >
