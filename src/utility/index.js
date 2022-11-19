30693afbc7414bbb9e8a2972b8948c1942ee55a7
Sasha Lehedza <sashalehedza@gmail.com>
2022-11-19 09:08:36 +0200

    fix

--- src/utility/index.js
+++ src/utility/index.js
@@ -8,10 +8,10 @@ export const excerpt = (str, count) => {
 export const discountCalc = (price, discount, count) => {
   return Number(discount) && Number(discount) !== 0
     ? Number(count) > 2
-      ? (Number(price) - Number(discount)) * Number(count) * 0.9
+      ? (Number(price) - Number(discount)) * Number(count) * ((100 - 10) / 100)
       : (Number(price) - Number(discount)) * Number(count)
     : Number(count) > 2
-    ? Number(price) * Number(count) * 0.9
+    ? Number(price) * Number(count) * ((100 - 10) / 100)
     : Number(price) * Number(count)
 }
 
